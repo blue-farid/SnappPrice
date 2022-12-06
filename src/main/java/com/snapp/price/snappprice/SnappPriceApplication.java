cef547b2d27a5d3714f1f6d6a10e08f716c0edb1
blue_farid <blue_farid@hotmail.com>
2022-12-06 11:16:46 +0330

    initial commit

---
+++ src/main/java/com/snapp/price/snappprice/SnappPriceApplication.java
@@ -0,0 +1,13 @@
+package com.snapp.price.snappprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnappPriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnappPriceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snapp/price/snappprice/SnappPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapp.price.snappprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnappPriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
