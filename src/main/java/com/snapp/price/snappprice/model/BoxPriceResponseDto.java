e696c9fe7b2c72aa718ed4d2fbe3a9355d80b8a4
blue_farid <blue_farid@hotmail.com>
2022-12-09 12:09:36 +0330

    add controller test

--- src/main/java/com/snapp/price/snappprice/model/BoxPriceResponseDto.java
+++ src/main/java/com/snapp/price/snappprice/model/BoxPriceResponseDto.java
@@ -1,12 +1,16 @@
 package com.snapp.price.snappprice.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
 @Data
+@Builder
+@NoArgsConstructor
 public class BoxPriceResponseDto implements Serializable {
     private String priceAmount;
 }
---
+++ src/test/java/com/snapp/price/snappprice/controller/PriceControllerTest.java
@@ -0,0 +1,72 @@
+package com.snapp.price.snappprice.controller;
+
+import com.snapp.price.snappprice.model.BoxPriceResponseDto;
+import com.snapp.price.snappprice.model.Location;
+import com.snapp.price.snappprice.service.PricingService;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.MessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.Locale;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@ActiveProfiles("test")
+class PriceControllerTest {
+
+    @MockBean
+    PricingService pricingService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    MessageSource messageSource;
+
+    Locale locale = Locale.ENGLISH;
+
+    @Test
+    @SneakyThrows
+    void getBoxPrice_ok() {
+        // given
+        given(pricingService.getBoxPrice(any(Location.class), any(Location[].class))).
+                willReturn(BoxPriceResponseDto.builder().priceAmount("40000").build());
+
+
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host("localhost")
+                .port(8081)
+                .path("/price/box")
+                .queryParam("sourceX", "1.0")
+                .queryParam("sourceY", "1.0")
+                .queryParam("destinationX", List.of("2.0"))
+                .queryParam("destinationY", List.of("2.0"))
+                .build();
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get(uriComponents.toUriString())
+                .accept(MediaType.APPLICATION_JSON);
+
+        // when then
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.priceAmount").value("40000"))
+                .andExpect(jsonPath("$.message").value(messageSource.getMessage("get.success", null, locale)));
+    }
+}
